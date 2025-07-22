2ff2ebca0dea8c0a3c12ef4966115483c37c9072
furkanerdogan <furkanerdd1@gmail.com>
2025-07-22 15:07:29 +0300

    Section 2 : Fault Tolerance-fallback methods with CircuitBreaker

--- library-service/src/main/java/com/kitaplik/library_service/LibraryServiceApplication.java
+++ library-service/src/main/java/com/kitaplik/library_service/LibraryServiceApplication.java
@@ -1,8 +1,12 @@
 package com.kitaplik.library_service;
 
+import com.kitaplik.library_service.client.RetrieveMessageErrorDecoder;
+import feign.Logger;
+import feign.codec.ErrorDecoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -11,5 +15,18 @@ public class LibraryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryServiceApplication.class, args);
 	}
+
+	/*
+	Feign Client Error Handling
+
+	@Bean
+	public ErrorDecoder errorDecoder(){
+		return new RetrieveMessageErrorDecoder();
+	}
+
+	@Bean
+	Logger.Level feignLoggerLevel(){return Logger.Level.FULL;}
+
+	 */
 
 }
--- library-service/src/main/java/com/kitaplik/library_service/client/BookServiceClient.java
+++ library-service/src/main/java/com/kitaplik/library_service/client/BookServiceClient.java
@@ -2,7 +2,11 @@ package com.kitaplik.library_service.client;
 
 import com.kitaplik.library_service.model.dto.BookDto;
 import com.kitaplik.library_service.model.dto.BookIdDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,15 +17,28 @@ import java.util.UUID;
 @FeignClient(name = "book-service",path = "/v1/book")
 public interface BookServiceClient {
 
+    Logger LOGGER = LoggerFactory.getLogger(BookServiceClient.class);
     @GetMapping
      ResponseEntity<List<BookDto>> getAllBook();
 
 
     @GetMapping("/isbn/{isbn}")
+    @CircuitBreaker(name = "getBookByIsbnCircuitBreaker",fallbackMethod = "getBookFallBack")
     ResponseEntity<BookIdDto> getBookByIsbn(@PathVariable("isbn")  String isbn);
+
+    default ResponseEntity<BookIdDto> getBookFallBack(@PathVariable("isbn") String isbn,Exception exception) {
+        LOGGER.info("Book not found by isbn + "+ isbn + "returning default BookIdDto object");
+        return ResponseEntity.ok(new BookIdDto(UUID.randomUUID(),"default-isbn"));
+    }
 
 
     @GetMapping("/book/{id}")
+    @CircuitBreaker(name = "getBookByIdCircuitBreaker", fallbackMethod = "getBookByIdFallback")
     ResponseEntity<BookDto> getBookById(@PathVariable("id")  UUID id);
+
+    default ResponseEntity<BookDto> getBookByIdFallback(UUID id, Exception exception) {
+        LOGGER.info("Book not found by id " + id + ", returning default BookDto object");
+        return ResponseEntity.ok(new BookDto(UUID.randomUUID(), "",0,"",""));
+    }
 
 }
